4ee76c6ccff2f49217b0e9022135b07f94dde86e
alexandra <shulga.sasha12@gmail.com>
2021-02-04 21:50:53 +0200

    basic Java Script task

--- test/intro/test.js
+++ test/intro/test.js
@@ -7,3 +7,60 @@ var b = {
     key4: 'value4',
     key5: 'value5'
 }
+
+/-----------------My Homework-----------------/
+
+var entrance = {
+    login: "admin",
+    password: "pass",
+}
+
+var registrarion = {
+    city: "Ukraine",
+    country: "Kyiv",
+    eMail: "shulga.sasha12@gmail.com",
+    number: 0954907879,
+    male: "woman",
+    age: 23,
+    name: "Alexandra",
+    surname: "Shulga",
+    agreementWithTheRules: true
+}
+
+var gym_information = {
+    abonForOneMounth: true,
+    abonForOneYear: true,
+    abonForHalfYear: false,
+    timeAllDay: true,
+    timeHAlfDay: false,
+    ageCategoryUntil: 50,
+    trainerForGym: true,
+    trainerForStretching: true,
+    trainerForYogga: false,
+    shower: true,
+    towels: true,
+    males: 'women/man',
+    openning: 08.00,
+    closing: 23.00
+}
+
+var sneakers = {
+    model: 'Adidas Streetball',
+    size: 38,
+    code: 'FV5147',
+    adress: 'ТРЦ ART MALL 2-й поверх, вул. Академіка Заболотного, 37, Київ',
+    index: 02000,
+    tefone: 095 185 4526,
+    availability: true,
+    null
+}
+
+var cats = ['exotic', 'persian' , 'semi-longhair', 'shorthair', 'somali', 'oriental' ]
+
+var табак = ['Asti',18.5 , 'Balli',11 ,'Arawak',2.3 , 'Bonga', 5, 'Daily Hookah', 8, 'Gixom', 16, 'Honey Badger', 2.6]
+
+var colors = ['red', 'yellow', 'brown', 'orange', 'green', null]
+
+var cars = ['Acura', 'Bentley', 'Shkoda', 'Audi', 'Renault',]
+
+var top_pop_singer = ['Ariana Grande', true, 'Travis Skott', false, 'The Weeknd', true, 'Saweetie', true, 'Megan Three Station', true]
