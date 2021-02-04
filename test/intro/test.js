903ff535c86e31dee29012f8ecbb8b1e9e1034b0
igorbr <igor.bronovytskyi@playtech.com>
2021-02-04 22:21:12 +0200

    fix js

--- test/intro/test.js
+++ test/intro/test.js
@@ -8,7 +8,8 @@ var b = {
     key5: 'value5'
 }
 
-/-----------------My Homework-----------------/
+/*-----------------Многострочный комментарий-----------------*/
+//Однострочный комментарий
 
 var entrance = {
     login: "admin",
@@ -19,7 +20,7 @@ var registrarion = {
     city: "Ukraine",
     country: "Kyiv",
     eMail: "shulga.sasha12@gmail.com",
-    number: 0954907879,
+    number: '0954907879',
     male: "woman",
     age: 23,
     name: "Alexandra",
@@ -40,8 +41,8 @@ var gym_information = {
     shower: true,
     towels: true,
     males: 'women/man',
-    openning: 08.00,
-    closing: 23.00
+    openning: '8:00',
+    closing: '23:00'
 }
 
 var sneakers = {
@@ -49,18 +50,46 @@ var sneakers = {
     size: 38,
     code: 'FV5147',
     adress: 'ТРЦ ART MALL 2-й поверх, вул. Академіка Заболотного, 37, Київ',
-    index: 02000,
-    tefone: 095 185 4526,
+    index: '02000',
+    tefone: '095 185 4526',
     availability: true,
-    null
 }
 
 var cats = ['exotic', 'persian' , 'semi-longhair', 'shorthair', 'somali', 'oriental' ]
 
-var табак = ['Asti',18.5 , 'Balli',11 ,'Arawak',2.3 , 'Bonga', 5, 'Daily Hookah', 8, 'Gixom', 16, 'Honey Badger', 2.6]
+var tobaco = [
+    {
+        name: 'Asti',
+        count: 18.5
+    },
+    {
+        name: 'Balli',
+        count: 11
+    },
+    {
+        name: 'Arawak',
+        count: 2.3
+    },
+    {
+        name: 'Bonga',
+        count: 5
+    },
+    {
+        name: 'Daily Hookah',
+        count: 8
+    },
+    {
+        name: 'Gixom',
+        count: 16
+    },
+    {
+        name: 'Honey Badger',
+        count: 2.6
+    }
+]
 
-var colors = ['red', 'yellow', 'brown', 'orange', 'green', null]
+var colors = ['red', 'yellow', 'brown', 'orange', 'green']
 
-var cars = ['Acura', 'Bentley', 'Shkoda', 'Audi', 'Renault',]
+var cars = ['Acura', 'Bentley', 'Shkoda', 'Audi', 'Renault']
 
 var top_pop_singer = ['Ariana Grande', true, 'Travis Skott', false, 'The Weeknd', true, 'Saweetie', true, 'Megan Three Station', true]
